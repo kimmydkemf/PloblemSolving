9b0f64d03063144568e42c45671e01267a3f7b63
selordoun <kimmydkemf@gmail.com>
2020-12-05 18:50:43 +0900

    12.5 알고리즘 공부 - BJ11052

---
+++ src/나혼자한다/백준강의DP_BJ1052.java
@@ -0,0 +1,21 @@
+package 나혼자한다;
+import java.util.*;
+
+public class 백준강의DP_BJ1052 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int []p=new int[N+1];
+		long []dp=new long [N+1];
+		for(int i=1;i<=N;i++) {
+			p[i]=sc.nextInt();
+			dp[i]=p[i];
+			for(int j=1;j<=i;j++) {
+				dp[i]=Math.max(dp[i],dp[i-j]+p[j]);
+			}
+		}
+		System.out.println(dp[N]);
+	}
+
+}
