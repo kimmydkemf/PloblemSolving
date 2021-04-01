8d3dd3fbc62fd42a7845cc16b175621e81a7324c
selordoun <kimmydkemf@gmail.com>
2021-04-01 19:19:27 +0900

    21.4.1 - 알고리즘공부 - BJ9655_돌게임.

---
+++ src/백준강의자료DP/돌게임.java
@@ -0,0 +1,14 @@
+package 백준강의자료DP;
+import java.util.*;
+
+public class 돌게임 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N%2==0) {
+			System.out.println("CY");
+		}else {
+			System.out.println("SK");
+		}
+	}
+}
