6d8ffcaf64f904e6745af146276d32aceba9bcf3
selordoun <kimmydkemf@gmail.com>
2021-04-06 13:56:32 +0900

    21.4.6 - 알고리즘공부 - 프로그래머스LEVEL1_행렬의덧셈.

---
+++ src/프로그래머스LEVEL1/행렬의덧셈.java
@@ -0,0 +1,16 @@
+package 프로그래머스LEVEL1;
+
+public class 행렬의덧셈 {
+	class Solution {
+	    public int[][] solution(int[][] arr1, int[][] arr2) {
+	        int[][] answer = {};
+	        answer=new int[arr1.length][arr1[0].length];
+	        for(int i=0;i<arr1.length;i++){
+	            for(int j=0;j<arr1[0].length;j++){
+	                answer[i][j]=arr1[i][j]+arr2[i][j];
+	            }
+	        }
+	        return answer;
+	    }
+	}
+}
